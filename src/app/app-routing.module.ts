a3cbf7baf7676665296225e603fbca1f3bcb5940
Dhanushka <dhanushka@akvasoft.com>
2018-09-21 11:53:08 +0530

    1st commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {ProductComponent} from './product/product.component';
+import {OrderComponent} from './order/order.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: DashboardComponent
+}, {
+  path: 'products',
+  component: ProductComponent
+},
+  {
+    path: 'place_new_order',
+    component: OrderComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TopbarComponent} from './topbar/topbar.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {ProductComponent} from './product/product.component';
+import {OrderComponent} from './order/order.component';
+import {MaterialModule} from './material/material.module';
+
+import {ProductService} from './shared/product.service';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    DashboardComponent,
+    ProductComponent,
+    OrderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
